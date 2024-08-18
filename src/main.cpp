a1558659d80dfa470adaaa9329bce4d2c80dbb58
Tommy <tommy.g.ciccone@gmail.com>
2024-08-17 22:00:46 -0400

    Remove old LCD Code

--- src/main.cpp
+++ src/main.cpp
@@ -65,10 +65,6 @@ void opcontrol() {
 
 
 	while (true) {
-		pros::lcd::print(0, "%d %d %d", (pros::lcd::read_buttons() & LCD_BTN_LEFT) >> 2,
-		                 (pros::lcd::read_buttons() & LCD_BTN_CENTER) >> 1,
-		                 (pros::lcd::read_buttons() & LCD_BTN_RIGHT) >> 0);  
-
 		// Tank Drive Control Scheme
 		int left = master.get_analog(ANALOG_LEFT_Y);   	 	// Gets Left Stick Up/Down Value
 		int right = master.get_analog(ANALOG_RIGHT_Y);  	// Gets Right Stick Up/Down Value
