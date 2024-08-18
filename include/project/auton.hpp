72178996120ff41872d2fd2900efd7bec536188d
Tommy <tommy.g.ciccone@gmail.com>
2024-08-17 22:03:22 -0400

    Remove old enum class (not used)

--- include/project/auton.hpp
+++ include/project/auton.hpp
@@ -3,11 +3,6 @@
 #include <string>                                          // Include string library
 
 namespace auton {                                          // Creates "auton" namespace
-    enum class AutonSelect : int {                         // List of autons to be selected for internal operations
-        disabled,
-        filler_routine
-    };
-
-const std::string autonNames =                             // List of autons to be selected for display
-    "Disabled\nFiller Routine\n";                           
+    const std::string autonNames =                             // List of autons to be selected for display
+        "Disabled\nFiller Routine\n";                           
 }
