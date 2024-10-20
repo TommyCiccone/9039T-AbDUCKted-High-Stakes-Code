f8ddb5bd5465460536318cdbd946f2179cc4cb67
Tommy <tommy.g.ciccone@gmail.com>
2024-10-20 14:53:05 -0400

    work on auton

--- include/project/auton.hpp
+++ include/project/auton.hpp
@@ -4,5 +4,5 @@
 
 namespace auton {                                          // Creates "auton" namespace
     const std::string autonNames =                         // List of autons to be selected for display
-        "Disabled\nRed Left\nRed Right\nBlue Left\nBlue Right\nClear Line";                           
+        "Disabled\nRed Left\nRed Right\nBlue Left\nBlue Right\nClear Line\nPID Testing";                           
 }
